fa11e257b2834f8d713bd3f89d8f170fc8687ece
NazeliQA <nazelihaobyan7@gmail.com>
2023-09-11 15:21:34 +0400

    init

---
+++ src/test/java/Homework20.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.lang.Thread;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class Homework20 {
+    public static void main(String[] args) {
+
+    }
+}
